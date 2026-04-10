29ffd9ef5b00cba83cce42ab9fb81abb012a91cc
MatheusDoRosario <matheusxdargws@gmail.com>
2026-04-10 11:15:03 -0300

    Implementa o controller

---
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -0,0 +1,52 @@
+package br.com.alurafood.pagamentos.controller;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService service;
+
+    @GetMapping
+    public Page<PagamentoDto> obterTodos(@PageableDefault(size = 10) Pageable pageable) {
+        return service.obterTodos(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDto> obterPorId(@PathVariable @NotNull Long id) {
+        var pagamento = service.obterPorId(id);
+        return ResponseEntity.ok(pagamento);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDto> criarPagamento(@RequestBody @Valid PagamentoDto dto, UriComponentsBuilder uriBuilder) {
+        var pagamentoCriado = service.criarPagamento(dto);
+        var uri = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamentoCriado.id()).toUri();
+
+        return ResponseEntity.created(uri).body(pagamentoCriado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PagamentoDto> atualizarPagamento(@PathVariable @NotNull Long id, @RequestBody @Valid PagamentoDto dto) {
+        var pagamentoAtualizado = service.atualizarPagamento(id, dto);
+        return ResponseEntity.ok(pagamentoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PagamentoDto> excluirPagamento(@PathVariable @NotNull Long id) {
+        service.excluirPagamento(id);
+        return ResponseEntity.noContent().build();
+    }
+}
